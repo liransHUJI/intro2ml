91112cd54f05e2b53d0bc5c5b971c82f30e4b516
liran2208 <65400001+liran2208@users.noreply.github.com>
2026-05-12 22:25:28 +0300

    ex2 files

---
+++ ex2/code/Classification/classifiers_comparison.py
@@ -0,0 +1,37 @@
+import itertools
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+### HELPER FUNCTIONS ###
+# Add here any helper functions which you think will be useful
+
+
+### Exercise Solution ###
+
+
+if __name__ == "__main__":
+
+    # Generate 3 different datasets - Moons, Circles and Two-Gaussians (80% train, 20% test)
+    pass
+
+    # For each dataset, train each one of the following 3 models: SVM with
+    # lambda = 5, Decision Tree with depth = 7, KNN with k = 5
+    pass
+
+    # Generate decision boundary plots
+    pass
+
+
+
+
+
+
